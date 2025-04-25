296a11a76e5a83c0ba6dabe475d48b8dd8f7cff7
venkateshpillala <153274380+venkateshpillala@users.noreply.github.com>
2025-04-25 20:04:43 +0530

    Add files via upload

--- src/main/java/com/loginlogout/comp/SameSiteCookieFilter.java
+++ src/main/java/com/loginlogout/comp/SameSiteCookieFilter.java
@@ -10,24 +10,24 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponseWrapper;
 
 @Component
 public class SameSiteCookieFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		
-		chain.doFilter(request, response);
-        if (response instanceof HttpServletResponse) {
-            HttpServletResponse res = (HttpServletResponse) response;
-            for (String header : res.getHeaders("Set-Cookie")) {
-                if (header.startsWith("JSESSIONID")) {
-                    res.setHeader("Set-Cookie", header + "; SameSite=None; Secure");
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpServletResponseWrapper responseWrapper = new HttpServletResponseWrapper(httpServletResponse) {
+            @Override
+            public void addHeader(String name, String value) {
+                if ("Set-Cookie".equalsIgnoreCase(name)) {
+                    value = value + "; SameSite=None";
                 }
+                super.addHeader(name, value);
             }
-        }
-
-	}
-
+        };
+        chain.doFilter(request, responseWrapper);
+    }
 }
